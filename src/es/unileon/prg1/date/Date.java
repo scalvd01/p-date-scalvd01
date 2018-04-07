b39009604fdfdaad49859524f959390aa77fabf5
scalvd01 <scalvd01@estudiantes.unileon.es>
2018-04-07 23:05:28 +0200

    modificacion

---
+++ src/es/unileon/prg1/date/Date.java
@@ -0,0 +1,394 @@
+package es.unileon.prg1.date;
+
+import java.util.Random;
+
+
+
+
+public class Date {
+	private int day;
+	private int month;
+	private int year;
+	
+	
+	public Date(int day, int month, int year) throws DateException{
+		this.year = year;
+		if (month<1 || month>12){
+				throw new DateException("Mes "+month+" no válido. Valores posibles entre 1 y 12");
+		}else{
+			this.month=month;
+		}
+		switch(this.month){
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+					if(day<1 || day>31){
+							throw new DateException("Dia "+day+" no válido en el mes "+month);
+					}else{
+						this.day = day;
+					}
+					break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+					if(day<1 || day>30){
+							throw new DateException("Dia "+day+" no válido en el mes "+month);
+					}else{
+						this.day = day;
+					}
+					break;
+			case 2:if(day<1 || day>28){
+							throw new DateException("Dia "+day+" no válido en el mes "+month);
+					}else{
+						this.day = day;
+					}
+					break;
+		}
+	}
+
+
+
+
+	public void setYear(int year){
+		this.year=year;
+	}
+	public int getYear(){
+		return this.year;
+	}
+	public void setMonth(int month){
+		this.month=month;
+	}
+	public int getMonth(){
+		return this.month;
+	}
+	public void setDay(int day){
+		this.day=day;
+	}
+	public int getDay(){
+		return this.day;
+	}
+	
+
+
+
+		//para comprobar si  dia mes y año son iguales en ambas fechas
+	boolean isSameYear(Date another){
+		if ( this.year == another.getYear() ){
+			return true;
+		}
+		return false;
+	}
+		
+
+	boolean isSameMonth(Date another){
+		if ( this.month == another.getMonth() ){
+			return true;
+		}
+		return false;
+	}
+
+
+	boolean isSameDay(Date another){
+		if ( this.day == another.getDay() ){
+			return true;
+		}
+		return false;
+	}
+	//si todo es igual a la vez
+	boolean isSame(Date another){
+		if( this.year == another.getYear() && this.month == another.getMonth() && this.day == another.getDay()){
+			return true;
+		}
+		return false;
+	}//simplificar los metodos sin if 
+	//falta
+
+
+
+
+	//para el nombre del mes
+	public String getMonthName(){
+
+		String monthName=null;
+
+		switch (this.month){
+
+			case 1: monthName = "Enero";
+					break;
+			case 2: monthName = "Febrero";
+					break;
+			case 3: monthName = "Marzo";
+					break;
+			case 4: monthName = "Abril";
+					break;
+			case 5: monthName = "Mayo";
+					break;
+			case 6: monthName = "Junio";
+					break;
+			case 7: monthName = "Julio";
+					break;
+			case 8: monthName = "Agosto";
+					break;
+			case 9: monthName = "Septiembre";
+					break;
+			case 10: monthName = "Octubre";
+					break;
+			case 11: monthName = "Noviembre";
+					break;
+			case 12: monthName = "Diciembre";
+					break;
+			default: monthName = "Mes inválido";
+					break;
+		}
+		return monthName;
+	}
+
+
+	//para si el dia del mes esta bien
+	public boolean isDayRight(){
+		if (((this.month==1) || (this.month==3) || (this.month==5) || (this.month==7) || (this.month==8) || (this.month==10) || (this.month==12)) && ((this.day<=31) && (this.day>0))){
+			return true;
+		}
+		else if((this.month==4) || (this.month==6) || (this.month==9) || (this.month==11) && ((this.day<=30) && (this.day>0))){
+			return true;
+		}
+		else if ((this.month==2) && ((this.day<=28) && (this.day>0))){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+
+
+	//para obtener las estaciones
+
+	public String getSeasonName(){
+		String seasonName=null;
+
+		switch(this.month){
+			case 1: seasonName = "Invierno";
+					break;
+			case 2: seasonName = "Invierno";
+					break;
+			case 3: if(this.day<21){
+						seasonName = "Invierno";
+					}
+					else{
+						seasonName = "Primavera";
+					}
+					break;
+			case 4: seasonName = "Primavera";
+					break;
+			case 5: seasonName = "Primavera";
+					break;
+			case 6: if(this.day<20){
+						seasonName = "Primavera";
+					}
+					else{
+						seasonName = "Verano";
+					}
+					break;
+			case 7: seasonName = "Verano";
+					break;
+			case 8: seasonName = "Verano";
+					break;
+			case 9: if(this.day<21){
+						seasonName = "Verano";
+					}
+					else{
+						seasonName = "Otoño";
+					}
+					break;
+			case 10:seasonName = "Otoño";
+					break;
+			case 11:seasonName = "Otoño";
+					break;
+			case 12:if(this.day<20){
+						seasonName = "Otoño";
+					}
+					else{
+						seasonName = "Invierno";
+					}
+					break;
+		}
+		return seasonName;
+	}
+
+
+
+
+	//para imprimir los meses restantes hasta final de año
+	public String getMonthsLeft(){
+		StringBuilder months = new StringBuilder();
+		int a = getMonth();
+		for (int i = a+1; i<=12; i++){
+
+			a = a + 1;
+			months.append(this.getMonthName()+" ");
+		}
+		return months.toString();
+	}
+			
+
+	//para imprimir la fecha
+	public String toString(){
+		return this.day + "/" + this.month + "/" + this.year;
+	}
+
+
+	//para imprimir todas las fechas hasta el final del mes
+	/*public String getDaysLeftOfMonth(){
+		StringBuilder daysLeft = new StringBuilder();
+		
+		while(this.isDayRight()==true){
+			daysLeft.append((this.day+1) +"/"+this.month+"/"+this.year);
+		}
+		if(this.isDayRight()==true){
+			do {
+				daysLeft.append((this.day+1) +"/"+this.month+"/"+this.year);
+
+			}while(this.isDayRight()==true);
+				
+			
+		}
+		return daysLeft.toString();
+	}*/
+
+	//para mostrar los meses con mismo n de dias 
+	public String getMonthsSameDays(){
+		String monthsSameDays=null;
+		switch(this.month){
+
+			case 1: monthsSameDays = "Marzo, Mayo, Julio, Agosto, Octubre, Diciembre";
+					break;
+			case 2: monthsSameDays = "(ningún otro)";
+					break;
+			case 3: monthsSameDays = "Enero, Mayo, Julio, Agosto, Octubre, Diciembre";
+					break;
+			case 4: monthsSameDays = "Junio, Septiembre, Noviembre";
+					break;
+			case 5: monthsSameDays = "Enero, Marzo, Julio, Agosto, Octubre, Diciembre";
+					break;
+			case 6: monthsSameDays = "Abril, Septiembre, Noviembre";
+					break;
+			case 7: monthsSameDays = "Enero, Marzo, Mayo, Agosto, Octubre, Diciembre";
+					break;
+			case 8: monthsSameDays = "Enero, Marzo, Mayo, Julio, Octubre, Diciembre";
+					break;
+			case 9: monthsSameDays = "Abril, Junio, Noviembre";
+					break;
+			case 10: monthsSameDays = "Enero, Marzo, Mayo, Julio, Agosto, Diciembre";
+					break;
+			case 11: monthsSameDays = "Abril, Junio, Septiembre";
+					break;
+			case 12: monthsSameDays = "Enero, Marzo, Mayo, Julio, Agosto, Octubre";
+					break;
+			
+		}
+		return monthsSameDays;
+	}
+
+
+	//para contar el num de dias que han pasado en una fecha
+	public int daysPast(){
+		int days=0;
+
+
+		switch(this.month){
+			case 1: days=days+getDay();
+					break; 
+			case 2: days=31+getDay();
+					break; 
+			case 3: days=59+getDay();
+					break;
+			case 4: days=90+getDay();
+					break;
+			case 5: days=120+getDay();
+					break;
+			case 6: days=151+getDay();
+					break;
+			case 7: days=181+getDay();
+					break;
+			case 8: days=212+getDay();
+					break;
+			case 9: days=243+getDay();
+					break;
+			case 10: days=273+getDay();
+					break;
+			case 11: days=304+getDay();
+					break;
+			case 12: days=334+getDay();
+					break;
+			
+		}
+		return days;
+	}
+
+
+
+	
+	public int numRandomTriesEqualDate1(){
+		int count=0;
+		Random random=new Random();
+		int randomDay=0;
+		int randomMonth=0;
+
+		while((this.day!=randomDay) || (this.month!=randomMonth)){
+			randomMonth=random.nextInt(13)+1;
+			randomDay=random.nextInt(32)+1;
+			count=count+1;
+		}
+		return count;
+
+	}
+
+	public int numRandomTriesEqualDate2(){
+		int count=0;
+		Random random=new Random();
+		int randomDay=0;
+		int randomMonth=0;
+		do{
+			randomMonth=random.nextInt(13)+1;
+			randomDay=random.nextInt(32)+1;
+			count=count+1;
+		}while((this.day!=randomDay) || (this.month!=randomMonth));
+
+		return count;
+	}
+
+	public String dayOfWeek(){
+		String dia=null;
+		int resto=0;
+
+		
+	}
+
+
+}
+
+
+
+
+
+/*	hacer 3 getters**
+	hacer 3 setters**
+	8 metodos is.Same: 4 con if** y 4 sin if
+	getMonthName**
+	isDayRight (int dia)**
+	getSeasonName**
+	getMonthLeft**
+	toString**
+	getDaysLeftOfMonth
+	getMonthsSameDays**
+	daysPast**
+	numRandomTriesEqualDate 1 con while**
+	numRandomTriesEqualDate 2 con do while**
+	dayOfWeek
+*/
